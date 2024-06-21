a3b3b3cfbe58c5f2800acd1ae050e76b12e3e6ce
LeandroIceta <iceta4@hotmail.com>
2024-06-21 11:24:03 -0300

    version 0.27

--- main.js
+++ main.js
@@ -6,10 +6,11 @@ let caracteristicasGerente = [7, 4, 6, 7];
 let caracteristicasVendedor = [9, 4, 7, 4];
 let caracteristicasContador = [3, 7, 5, 9];
 let caracteristicasPasante = [6, 5, 9, 4];
-/* Características de los personajes: carisma, conocimiento, ética, excel. */
+/* Características de los personajes: carisma, conocimiento, motivación, excel. */
 
 let seleccionUsuario;
 let caracteristicas;
+let tipoPersonaje;
 
 while (true) {
     seleccionUsuario = prompt("Ahora, elige tu tipo de personaje:\n1. Gerente (Carisma 7, Ética 4, Motivación 6, Excel 7)\n2. Vendedor (Carisma 9, Ética 4, Motivación 7, Excel 4)\n3. Contador (Carisma 3, Ética 7, Motivación 5, Excel 9)\n4. Pasante (Carisma 6, Ética 5, Motivación 9, Excel 4)");
@@ -38,7 +39,28 @@ while (true) {
 
     break;
 }
-
 console.log(nombre, tipoPersonaje);
 
 alert(`Has elegido ser ${tipoPersonaje}. ¡A trabajar!\nTus características son:\nCarisma: ${caracteristicas[0]}\nÉtica: ${caracteristicas[1]}\nMotivación: ${caracteristicas[2]}\nExcel: ${caracteristicas[3]}`);
+
+
+let dado = Math.floor(Math.random() * 13)-6;
+alert ("Recién llegas a la oficina y te das cuenta que hay mucha gente alrededor del dispenser de agua.\nAL parecer estan hablando de la nueva implementación de IA y quien va a ser remplazado. \nSi queres averiguar más, tira un dado y si sumado a tu carisma da más de 8 te enteras del plan y sumas un punto a conocimiento. \nSi fallas restas un punto de motivación")
+
+const confirmacion = confirm("Tira el dado");
+console.log("Resultado del dado:", dado);
+
+const resultadoTotal = dado + caracteristicas[0];
+
+if (resultadoTotal >= 8) {
+        caracteristicas[0]++;
+        alert("¡Te enteraste del plan! Suma 1 punto a conocimiento.");}
+ else {
+        caracteristicas[2]--;
+        alert("Tus compañeros te miran raro y se cierran en un circulo dejandote afuera. Restas 1 punto de motivación.");
+    }
+
+
+alert ("mas contenido en la proxima versión")
+
+
